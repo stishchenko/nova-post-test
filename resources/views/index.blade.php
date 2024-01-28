fa4f39ca33ac0c547c1d536d42da2e06a964ab83
Sofiia Tishchenko <sofiia.tishchenko@asd.team>
2024-01-28 19:53:25 +0100

    Refactor appearance to use layout extending

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Nova Post</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-            crossorigin="anonymous"></script>
-    <style>
-        .form-select:focus, .form-control:focus {
-            box-shadow: none;
-        }
-    </style>
-</head>
-
-<body>
-<div class="container">
-
+@extends('app')
+@section('content')
     <div class="row">
         <div class="col-1">
             <select id="locale-select" class="form-select">
@@ -93,6 +71,4 @@
             </form>
         </div>
     </div>
-</div>
-
-</body>
+@endsection
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <title>Nova Post</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-            crossorigin="anonymous"></script>
-</head>
-
-<body>
-<div class="container">
-
+@section('content')
     <h4>{{ __('count_cost_header') }}</h4>
 
     <div class="row mt-3">
@@ -39,6 +22,4 @@
            class="btn btn-outline-dark"><i class="bi bi-arrow-left"></i>
             {{ __('back_to_index') }}</a>
     </div>
-</div>
-
-</body>
+@endsection
